5834bc09273a8b8ad0eb93e927beee324a740533
adamesh <adameshh@gmail.com>
2017-06-15 00:04:18 -0400

    got this far

--- lib/student.rb
+++ lib/student.rb
@@ -1,6 +1,42 @@
+require 'Sqlite3'
+
+
 class Student
 
   # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]  
-  
+  #  with DB[:conn]
+
+  attr_accessor :name, :grade
+  attr_reader :id
+
+  def initialize(name, grade, id=(nil))
+    @name = name
+    @grade = grade
+    @id = id
+  end
+
+  def self.create_table
+    sql = "CREATE TABLE students (
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      grade TEXT
+    );"
+    DB[:conn].execute(sql)
+  end
+
+  def self.drop_table
+    sql = "DROP TABLE students;"
+    DB[:conn].execute(sql)
+  end
+
+  def save
+    sql = "INSERT INTO students (name, grade)
+    VALUES ('#{self.name}', '#{self.grade}')"
+    DB[:conn].execute(sql)
+    sql = "SELECT id FROM students ORDER BY id DESC LIMIT 1;"
+    @id = DB[:conn].execute(sql).first.first # I don't like the @id here
+  end
+
+
+
 end
